d72059caba3c7b61340d952e11d9c820d3f3a141
Karan kendre <karankendreg@gmail.com>
2025-02-27 23:00:10 +0530

    Updated Code

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ArrowUpRight, ChevronDown } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import ProjectModal from './ProjecttModel';
